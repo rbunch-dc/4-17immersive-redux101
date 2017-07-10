3a40d2ef0b34349a97efb1f5a08ded7bd0983e70
Robert Bunch <rob@digitalcrafts.com>
2017-07-10 12:33:18 -0400

    init app. setup store and provider in index

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Students from './Students';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Students />
+      </div>
+    );
+  }
+}
+
+export default App;
